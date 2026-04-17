8bcbf578f4bc1e053ffa2c929a4dea428cafcc66
Bacalhau <louisquibeufworkspace@gmail.com>
2026-04-17 13:57:51 +0200

    ajout d'une vidéo + frames, utilisation de 5 frames pour la création des grands nombres avec un random

--- src/main.py
+++ src/main.py
@@ -4,13 +4,13 @@ import os
 from chiffrement_dechiffrement.rsa_cles   import generer_cles_rsa
 from chiffrement_dechiffrement.cryptage   import chiffrer
 from chiffrement_dechiffrement.decryptage import dechiffrer
-from number_generator.setup import image_to_bytes, bytes_to_grands_entiers
+from number_generator.setup import images_to_bytes, bytes_to_grands_entiers
 
-PHOTO_PATH = os.path.join("docs", "photo_lava_lamp.jpg")
+PHOTO_PATH = os.path.join("docs", "Pictures")
 
 if __name__ == "__main__":
     # A remplacer par le generateur
-    raw_bytes = image_to_bytes(PHOTO_PATH)
+    raw_bytes = images_to_bytes(PHOTO_PATH)
     nombre_1, nombre_2 = bytes_to_grands_entiers(raw_bytes)
 
     # 1. Génération des clés RSA
